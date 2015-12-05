0635aa3d487e2e48eb9b1aa764c731f91210bad7
Georg Kasper <MdaaaaO>
2015-12-05 14:28:40 -0800

    refactoring, comments added

--- python/url_2_country.py
+++ python/url_2_country.py
@@ -1,11 +1,17 @@
-'''
+#!/usr/bin/env python
+"""
 Created on Dec 5, 2015
 
 This product includes GeoLite2 data created by MaxMind, available from
 <a href="http://www.maxmind.com">http://www.maxmind.com</a>.
 
+Example:
+    
+    $ python3 url_2_country.py -u www.hft-stuttgart.de
+    URL: www.hft-stuttgart.de, IP: 193.196.136.111, COUNTRY: Germany, CITY: Stuttgart
+
 @author: mdaaaa
-'''
+"""
 
 import geoip2.database
 import socket
@@ -13,24 +19,38 @@ import pandas
 import sys
 import argparse
 
-csv_file = 'csv/top-1m.csv'
-
 try:
     country_reader = geoip2.database.Reader('db/GeoLite2-Country.mmdb')
     city_reader = geoip2.database.Reader('db/GeoLite2-City.mmdb')
 except (OSError, IOError) as e:
     print(e)
-    print('GeoLite2 Free Downloadable Databases: http://dev.maxmind.com/geoip/geoip2/geolite2/')
-    
+    print('GeoLite2 Free Downloadable Databases: \
+            http://dev.maxmind.com/geoip/geoip2/geolite2/')
 
 def getIPfromURL(url):
+    """"
+    
+    Args:
+        url (string): like 'hft-stuttgart.de' or 'www.hft-stuttgart.de'
+    
+    Returns:
+        ip (string): Format '193.196.136.111'
+        None: If no connection can be established
+    
+    Example:
+        >>> getIPfromURL('hft-stuttgart.de')
+        '193.196.136.111'
+        
+    """
     ip = None
     try:
         ip = socket.gethostbyname(url)
     except socket.error as e:
-        pass
+        print(str(url) + ': ' + str(e))
+        return ip
     
     return ip
+        
         
 def getCountryByIP(ip):
     country_name = None
@@ -44,6 +64,7 @@ def getCountryByIP(ip):
    
     return country_name
 
+
 def getCityByIP(ip):
     city_name = None
     
@@ -56,9 +77,14 @@ def getCityByIP(ip):
         
     return city_name
 
-def open_with_pandas_read_csv(csv_file):
-    
-    df = pandas.read_csv('csv/top-1m.csv', sep=',')
+
+def open_with_pandas_read_csv():
+    """
+    read and write example using csv, not used at all but maybe useful for 
+    further development.
+    """
+    csv_file = 'csv/top-1m.csv'
+    df = pandas.read_csv(csv_file, sep=',')
     data = df.values
     urls = []
     broken_urls = []
@@ -79,42 +105,46 @@ def open_with_pandas_read_csv(csv_file):
     
 
 class TerminalMenu:
+    """
+    TerminalMenu is used as a handler for sys.argv inputs. 
+    """
     
     @staticmethod
     def foo(urls):
-            for u in urls:
-                ip = getIPfromURL(url=u)
+        """
+        checking all input URLs for their IP, COUNTRY, CITY - 
+        If connection can be established this method will print the output to
+        the shell.
+        """
+        for u in urls:
+            ip = getIPfromURL(url=u)
+            if ip is not None:
                 country = getCountryByIP(ip)
                 city = getCityByIP(ip)
-                
-                print('#'  
-                      + ' | url: ' + str(u) 
-                      + ' | ip: ' + str(ip) 
-                      + ' | country: ' + str(country)
-                      + ' | city: ' + str(city))
-                
+                print('URL: ' + str(u)
+                      + ', IP: ' + str(ip) 
+                      + ', COUNTRY: ' + str(country)
+                      + ', CITY: ' + str(city))
+    
+                        
     def __init__(self, sys_argv):
-        """ The constructor of TerminalMenu:
-        This constructor takes the arguments from the command line, use -h for more help.
-        :param sys_argv:
-        :return:
         """
-        self.parser = argparse.ArgumentParser(description="Input URLs to get host-IP, host-Country and host-City.")
+        initialization for shell inputs / handling
+        """
 
-        # When parse_args() is called, argparse will make sure that only one of the arguments in the mutually
-        # exclusive group was present on the command line
+        self.parser = argparse.ArgumentParser(description="Input URLs to get \
+            host-IP, host-Country and host-City.")
         self.group = self.parser.add_mutually_exclusive_group()
-        # Container 1 (only one allowed)
-        self.group.add_argument("-u", "--url", nargs="+", type=str, help="input URLs, separate by space.")
-        
+        self.group.add_argument("-u", "--url", nargs="+", type=str, help="input\
+             URLs, separate by space.")
         args = self.parser.parse_args()
 
         if len(sys_argv) > 1:
-
             if args.url:
                 TerminalMenu.foo(urls=args.url)                                                  
         else:
             self.parser.print_help()
+
 
 def main():
     TerminalMenu(sys_argv=sys.argv)
