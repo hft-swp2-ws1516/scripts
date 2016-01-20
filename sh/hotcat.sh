4acc7cb11ef691c4dfb26afb155a5057da70d91d
Georg Kasper <georg.kasper08@gmail.com>
2016-01-20 07:53:45 -0800

    refactoring: removed spam

--- sh/hotcat.sh
+++ sh/hotcat.sh
@@ -23,7 +23,7 @@ CheckURL() {
 CreateDIR() {
     if [ ! -d "$1" ]
     then
-        mkdir "$1"
+        sudo mkdir "$1"
         echo "  $1 ${YELLOW}...created!${DEFAULT}"
     else
         echo "  $1 ${GREEN}...exists!${DEFAULT}"
@@ -32,7 +32,8 @@ CreateDIR() {
 
 # check whether program is installed
 CheckIfInstalled() {
-    command -v $1 >/dev/null 2>&1 || { echo >&2 "  ${DEFAULT}I require ${YELLOW}$1 ${DEFAULT}but it's not installed. ${RED}Aborting. ${DEFAULT}"; exit 1; }
+    command -v $1 >/dev/null 2>&1 || { echo >&2 "  ${DEFAULT}I require ${YELLOW}$1 ${DEFAULT}but it's not installed. ${RED}Aborting.${DEFAULT}"; 
+    exit 1; }
 }
 
 # Clone latest version from GitHub
@@ -42,11 +43,23 @@ CloneFromGitHub() {
     if [ "$(ls -A $2)" ]; then
         echo "  ${YELLOW}Pulling from ${DEFAULT}$1${BLUE}"
         cd $2
-        echo -n "    "; git pull
+        echo -n "    ";sudo git pull
     else
         echo "  ${YELLOW}Cloning from ${DEFAULT}$1${BLUE}"
-        echo -n "    "; git clone $1 $2 
+        echo -n "    ";sudo git clone $1 $2 
     fi
+}
+
+AptGetInstall() {
+    echo -n "${DEFAULT}apt-get install ${YELLOW}$1" 
+    sudo apt-get install $1 > /dev/null
+    echo " ${GREEN}done!${DEFAULT}"    
+}
+
+PipInstall() {
+    echo -n "${DEFAULT}pip install ${YELLOW}$1"
+    sudo pip install $1 > /dev/null 
+    echo " ${GREEN}done!${DEFAULT}"    
 }
 
 # set github project links 
@@ -84,44 +97,63 @@ CloneFromGitHub $crawler_link $crawler_dir
 CloneFromGitHub $website_link $website_dir
 echo "${DEFAULT}"
 
-# extend / update list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927 2>/dev/null
-echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list 
 
-# update packages
-echo -n "${BLUE}Update packages${DEFAULT}"; apt-get update > /dev/null; echo " ${GREEN}done!${DEFAULT}"
+echo "${GREEN}>>> APT-GETS <<<${DEFAULT}"
+# Mongo-DB
+{
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927 
+} > /dev/null 2>&1
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list > /dev/null 
+echo -n "${DEFAULT}Update packages${DEFAULT}";sudo apt-get update > /dev/null; echo " ${GREEN}done!${DEFAULT}"
 
 # apt-get install
-echo -n "${BLUE}Install mongodb-org${DEFAULT}"; apt-get install -y mongodb-org > /dev/null ; echo " ${GREEN}done!${DEFAULT}"
-echo -n "${BLUE}Install apache2${DEFAULT}"; apt-get install apache2 > /dev/null ; echo " ${GREEN}done!${DEFAULT}"
-echo -n "${BLUE}Install npm${DEFAULT}"; apt-get install npm > /dev/null ; echo " ${GREEN}done!${DEFAULT}"
-echo -n "${BLUE}Install python-pip${DEFAULT}"; apt-get install python-pip > /dev/null ; echo " ${GREEN}done!${DEFAULT}"
-echo
+AptGetInstall 'mongodb-org'
+AptGetInstall 'python-pip'
+AptGetInstall 'npm'
+AptGetInstall 'apache2'
+AptGetInstall 'node'
 
 # db_api specific 
-echo "${GREEN}>>> DB_API <<<"
+echo "${GREEN}>>> DB_API <<< ${DEFAULT}"
 cd $db_api_dir
-npm install 2>/dev/null
-echo "${GREEN}done!"
-echo
+echo -n "${DEFAULT}npm install"
+sudo npm install 2>/dev/null
+echo " ${GREEN}done!${DEFAULT}"    
+cd
 
 # crawler specific
-echo "${BLUE}>>> CRAWLER <<<"
+echo "${GREEN}>>> CRAWLER <<<${DEFAULT}"
 cd $crawler_dir
-pip install tornado >/dev/null
-pip install pymongo >/dev/null
-pip install tld >/dev/null
-pip install settings >/dev/null
+PipInstall 'pymongo' 
+PipInstall 'request'
+PipInstall 'tornado'
+PipInstall 'tld'
+PipInstall 'lxml'
+cd
 #export PYTHONPATH=/home/hotcat/HotCat/crawler/
-echo "${BLUE}done!"
-echo
 
 # website specific
-echo "${YELLOW}>>> WEBSITE <<<"
+echo "${GREEN}>>> WEBSITE <<<${DEFAULT}"
 cd $website_dir
-npm install --global gulp > /dev/null
-npm install --save gulp-uglify > /dev/null
-npm install > /dev/null
-gulp build > /dev/null
-echo "${YELLOW}done!"
+
+npm install --global gulp 2>/dev/null
+npm install --save gulp-uglify 2>/dev/null
+
+echo -n "${DEFAULT}npm install"
+{
+    sudo npm install
+} > /dev/null 2>&1
+echo " ${GREEN}done!${DEFAULT}"    
+
+echo -n "${DEFAULT}gulp build"
+{
+    sudo gulp build
+} > /dev/null 2>&1
+echo " ${GREEN}done!${DEFAULT}"    
+
+sudo gulp build 2>/dev/null
+cd
+
+# Doublecheck List
+CheckIfInstalled 'mongo'
 echo ${DEFAULT}
