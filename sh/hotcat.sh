20b9fc760a35de434e65793f7c28ae12126499ef
Georg Kasper <georg.kasper08@gmail.com>
2016-01-21 00:20:14 -0800

    fixed folder bug, new style

--- sh/hotcat.sh
+++ sh/hotcat.sh
@@ -13,20 +13,19 @@ clear
 CheckURL() {
     if curl --output /dev/null --silent --head --fail "$1" 
     then
-        echo "  $1 ${GREEN}...ok!${DEFAULT}"
+        echo -n
+        #echo "  $1 ${GREEN}...ok!${DEFAULT}"
     else
         echo "  $1 ${RED}...fail!${DEFAULT}"
-    fi
+        exit 1
+    fi 
 }
 
 # create directories
 CreateDIR() {
     if [ ! -d "$1" ]
-    then
-        sudo mkdir "$1"
-        echo "  $1 ${YELLOW}...created!${DEFAULT}"
-    else
-        echo "  $1 ${GREEN}...exists!${DEFAULT}"
+    then 
+        sudo install -d -m 0777 $1
     fi
 }
 
@@ -41,39 +40,45 @@ CloneFromGitHub() {
     url=$1
     dir=$2 	    
     if [ "$(ls -A $2)" ]; then
-        echo "  ${YELLOW}Pulling from ${DEFAULT}$1${BLUE}"
+        echo -n "|- ${YELLOW}Pulling from ${DEFAULT}$1${DEFAULT}"
         cd $2
-        echo -n "    ";sudo git pull
+        sudo git pull > /dev/null
+        echo " ${GREEN}done!${DEFAULT}"
     else
-        echo "  ${YELLOW}Cloning from ${DEFAULT}$1${BLUE}"
-        echo -n "    ";sudo git clone $1 $2 
+        echo -n "|- ${YELLOW}Cloning from ${DEFAULT}$1${DEFAULT}"
+        sudo git clone $1 $2 --quiet> /dev/null
+        echo " ${GREEN}done!${DEFAULT}" 
     fi
 }
 
 AptGetInstall() {
-    echo -n "${DEFAULT}apt-get install ${YELLOW}$1" 
+    echo -n "|- ${DEFAULT}apt-get install ${YELLOW}$1" 
     sudo apt-get install $1 > /dev/null
     echo " ${GREEN}done!${DEFAULT}"    
 }
 
 PipInstall() {
-    echo -n "${DEFAULT}pip install ${YELLOW}$1"
+    echo -n "|- ${DEFAULT}pip install ${YELLOW}$1"
     sudo pip install $1 > /dev/null 
     echo " ${GREEN}done!${DEFAULT}"    
 }
 
+
+echo "|--------------------------------------------------------------------------------|"
+echo "|                                Installing HotCat                               |"
+echo "|--------------------------------------------------------------------------------|"
+echo "|                                                                                |"
 # set github project links 
-echo "Check URLs:"
+echo "|- Checking GitHub URLs.                                                         |"
 db_api_link=https://github.com/hft-swp2-ws1516/db-api.git
 CheckURL $db_api_link
 crawler_link=https://github.com/hft-swp2-ws1516/crawler.git
 CheckURL $crawler_link
 website_link=https://github.com/hft-swp2-ws1516/website.git
 CheckURL $website_link
-echo 
 
 # create directories:
-echo "Create directories:"
+echo "|- Createing directories.                                                        |"
 root_dir=$HOME'/HotCat/'
 CreateDIR $root_dir
 db_api_dir=$root_dir'db-api/'
@@ -84,27 +89,33 @@ website_dir='/var/www/HotCat/website/'
 CreateDIR '/var/www/'
 CreateDIR '/var/www/HotCat/'
 CreateDIR $website_dir
-echo 
+echo "|                                                                                |"
+echo "|--------------------------------------------------------------------------------|"
 
 # required
 git='git'
 CheckIfInstalled $git
 
 # pull latest from GitHub
-echo "Clone/Pull from GitHub:"
+echo "|                                                                                |"
+echo "|- GitHub:                                                                       |"
 CloneFromGitHub $db_api_link $db_api_dir
 CloneFromGitHub $crawler_link $crawler_dir
 CloneFromGitHub $website_link $website_dir
-echo "${DEFAULT}"
+echo -n "${DEFAULT}"
 
-
-echo "${GREEN}>>> APT-GETS <<<${DEFAULT}"
+echo "|                                                                                |"
+echo "|--------------------------------------------------------------------------------|"
+echo "|                                                                                |"
+echo "|- Installing packages:                                                          |"
 # Mongo-DB
 {
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927 
 } > /dev/null 2>&1
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list > /dev/null 
-echo -n "${DEFAULT}Update packages${DEFAULT}";sudo apt-get update > /dev/null; echo " ${GREEN}done!${DEFAULT}"
+echo -n "|- Update packages"; 
+sudo apt-get update > /dev/null; 
+echo " ${GREEN}done!${DEFAULT}"
 
 # apt-get install
 AptGetInstall 'mongodb-org'
@@ -114,15 +125,23 @@ AptGetInstall 'apache2'
 AptGetInstall 'node'
 
 # db_api specific 
-echo "${GREEN}>>> DB_API <<< ${DEFAULT}"
+echo "|                                                                                |"
+echo "|--------------------------------------------------------------------------------|"
+echo "|                                                                                |"
+echo "|- DB_API:                                                                       |"
 cd $db_api_dir
-echo -n "${DEFAULT}npm install"
-sudo npm install 2>/dev/null
+echo -n "${DEFAULT}|- npm install"
+{
+   sudo npm install 2>/dev/null
+} > /dev/null 2>&1
 echo " ${GREEN}done!${DEFAULT}"    
 cd
 
 # crawler specific
-echo "${GREEN}>>> CRAWLER <<<${DEFAULT}"
+echo "|                                                                                |"
+echo "|--------------------------------------------------------------------------------|"
+echo "|                                                                                |"
+echo "|- CRAWLER:                                                                      |"
 cd $crawler_dir
 PipInstall 'pymongo' 
 PipInstall 'request'
@@ -133,19 +152,22 @@ cd
 #export PYTHONPATH=/home/hotcat/HotCat/crawler/
 
 # website specific
-echo "${GREEN}>>> WEBSITE <<<${DEFAULT}"
+echo "|                                                                                |"
+echo "|--------------------------------------------------------------------------------|"
+echo "|                                                                                |"
+echo "|- WEBSITE:                                                                      |"
 cd $website_dir
 
 npm install --global gulp 2>/dev/null
 npm install --save gulp-uglify 2>/dev/null
 
-echo -n "${DEFAULT}npm install"
+echo -n "${DEFAULT}|- npm install"
 {
     sudo npm install
 } > /dev/null 2>&1
 echo " ${GREEN}done!${DEFAULT}"    
 
-echo -n "${DEFAULT}gulp build"
+echo -n "${DEFAULT}|- gulp build"
 {
     sudo gulp build
 } > /dev/null 2>&1
@@ -156,4 +178,17 @@ cd
 
 # Doublecheck List
 CheckIfInstalled 'mongo'
+echo "|                                                                                |"
+echo "|--------------------------------------------------------------------------------|"
+echo "|                                                                                |"
+echo "| HFT Stuttgart:   http://www.hft-stuttgart.de/                                  |"
+echo "| GitHub:          https://github.com/hft-swp2-ws1516                            |"
+echo "|                                                                                |"
+echo "|--------------------------------------------------------------------------------|"
 echo ${DEFAULT}
+
+
+
+
+
+
